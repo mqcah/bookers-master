6cdc26edcb69b02fc019f0d33a4474f1192cbda8
mqcah <mqcha94@outlook.jp>
2020-06-25 17:48:28 +0900

    [Finish]課題：アプリケーションを作成してみよう：応用編

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '5095d4b0493b8828d08dc6ace924cf8ec8d4d1e0b9fb50527dd8a10b49090ac11adab65fde6776a37539b0c01204e58c778e12c1fd146eaebcb99db9b612a1e9'
